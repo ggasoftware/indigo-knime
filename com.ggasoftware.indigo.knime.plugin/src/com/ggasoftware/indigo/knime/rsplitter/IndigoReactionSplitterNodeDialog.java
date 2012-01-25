13eb846a29f8d07bff54f3b34b5d212faeae1df2
Alexander Savelyev <asavelyev@ggasoftware.com>
2012-01-25 14:47:29 +0400

    reaction splitter settings were added

--- com.ggasoftware.indigo.knime.plugin/src/com/ggasoftware/indigo/knime/rsplitter/IndigoReactionSplitterNodeDialog.java
+++ com.ggasoftware.indigo.knime.plugin/src/com/ggasoftware/indigo/knime/rsplitter/IndigoReactionSplitterNodeDialog.java
@@ -1,8 +1,11 @@
 package com.ggasoftware.indigo.knime.rsplitter;
 
+import org.knime.core.data.DataTableSpec;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.NotConfigurableException;
 
 /**
  * <code>NodeDialog</code> for the "IndigoReactionSplitter" Node.
@@ -24,5 +27,10 @@ public class IndigoReactionSplitterNodeDialog extends NodeDialogPane {
    protected void saveSettingsTo(NodeSettingsWO settings)
          throws InvalidSettingsException {
       
+   }
+   
+   @Override
+   protected void loadSettingsFrom(NodeSettingsRO settings,
+         DataTableSpec[] specs) throws NotConfigurableException {
    }
 }
--- com.ggasoftware.indigo.knime.plugin/src/com/ggasoftware/indigo/knime/rsplitter/IndigoReactionSplitterSettings.java
+++ com.ggasoftware.indigo.knime.plugin/src/com/ggasoftware/indigo/knime/rsplitter/IndigoReactionSplitterSettings.java
@@ -1,5 +1,30 @@
 package com.ggasoftware.indigo.knime.rsplitter;
 
-public class IndigoReactionSplitterSettings {
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
 
+import com.ggasoftware.indigo.knime.IndigoNodeSettings;
+
+public class IndigoReactionSplitterSettings extends IndigoNodeSettings{
+   
+   public final SettingsModelString reactionColName = new SettingsModelString("reactionColName", null);
+   
+   public final SettingsModelString reactantColName = new SettingsModelString("reactantColName", "Reactants");
+   public final SettingsModelString productColName = new SettingsModelString("productColName", "Products");
+   public final SettingsModelString catalystColName = new SettingsModelString("catalystColName", "Catalysts");
+   
+   public final SettingsModelBoolean extractReactants = new SettingsModelBoolean("extractReactants", true);
+   public final SettingsModelBoolean extractProducts = new SettingsModelBoolean("extractProducts", true);
+   public final SettingsModelBoolean extractCatalysts = new SettingsModelBoolean("extractCatalysts", false);
+   
+   public IndigoReactionSplitterSettings() {
+      addSettingsParameter(reactionColName);
+      addSettingsParameter(reactantColName);
+      addSettingsParameter(productColName);
+      addSettingsParameter(catalystColName);
+      addSettingsParameter(extractReactants);
+      addSettingsParameter(extractProducts);
+      addSettingsParameter(extractCatalysts);
+   }
+   
 }
